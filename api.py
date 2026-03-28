0a427299ee1f26baa863375782bed15906079439
Mathys <mathyswl10@gmail.com>
2026-03-28 17:02:51 +0100

    update api

--- api.py
+++ api.py
@@ -2,34 +2,32 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-command = {}
+command = {"action": ""}
+
+SECRET = "MON_CODE_SECRET"
 
 @app.route("/")
 def home():
     return "API OK"
 
-@app.route("/set-command", methods=["POST"])
-def set_command():
-    global command
-    data = request.json
+@app.route("/lancer")
+def lancer():
+    key = request.args.get("key")
+    logiciel = request.args.get("logiciel")
 
-    command = {
-        "action": data.get("action"),
-        "value": data.get("value")
-    }
-
-    return jsonify({"status": "ok"})
-
+    if key == SECRET:
+        command["action"] = logiciel
+        return "ok"
+    return "refuse"
 
 @app.route("/command")
 def get_command():
     global command
 
     current = command.copy()
-    command = {}  # reset après lecture
+    command["action"] = ""  # reset après lecture
 
     return jsonify(current)
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
