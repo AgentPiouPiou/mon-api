7bac265b15af832efd54b9cca7b82fa057f88578
Mathys <mathyswl10@gmail.com>
2026-03-28 10:37:31 +0100

    update api

---
+++ api.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+command = {"action": ""}
+
+SECRET = "MON_CODE_SECRET"
+
+@app.route("/")
+def home():
+    return "API OK"
+
+@app.route("/lancer")
+def lancer():
+    key = request.args.get("key")
+    logiciel = request.args.get("logiciel")
+
+    if key == SECRET:
+        command["action"] = logiciel
+        return "ok"
+    return "refuse"
+
+@app.route("/command")
+def get_command():
+    return jsonify(command)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
