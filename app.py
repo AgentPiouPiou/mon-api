22f428d0021f6d08ae1d73b2a8f95c435b9e3e2a
Mathys <mathyswl10@gmail.com>
2026-04-03 23:31:09 +0200

    Mise à jour API

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ eventlet.monkey_patch()
 from flask import Flask
 from flask_socketio import SocketIO
 import time
-import pyautogui
 
 app = Flask(__name__)
 
@@ -12,7 +11,7 @@ socketio = SocketIO(
     app,
     cors_allowed_origins="*",
     async_mode="eventlet",
-    max_http_buffer_size=20 * 1024 * 1024,
+    max_http_buffer_size=100 * 1024 * 1024,
     ping_timeout=10,
     ping_interval=5
 )
@@ -20,7 +19,6 @@ socketio = SocketIO(
 connected = False
 last_signal_time = 0
 TIMEOUT = 2
-frames_data = {}
 
 # ---------------- STATUS ----------------
 @socketio.on("status")
@@ -29,31 +27,24 @@ def status(data):
     if data == "connected":
         connected = True
         last_signal_time = time.time()
+
     socketio.emit("update", {"connected": connected})
 
-# ---------------- FRAMES ----------------
+# ---------------- VIDEO ----------------
 @socketio.on("frames")
 def receive_frames(data):
-    global frames_data
-    frames_data = data
     socketio.emit("frames", data)
 
-# ---------------- MOUSE ----------------
-@socketio.on("mouse_move")
-def mouse_move(data):
-    try:
-        dx = data.get("dx", 0)
-        dy = data.get("dy", 0)
-        x, y = pyautogui.position()
-        pyautogui.moveTo(x + dx, y + dy, duration=0)
-    except Exception as e:
-        print("Erreur mouse_move:", e)
+# ---------------- SOURIS ----------------
+@socketio.on("move_mouse")
+def move_mouse(data):
+    socketio.emit("move_mouse", data)
 
 # ---------------- CHECK ----------------
 def check_loop():
     global connected, last_signal_time
     while True:
-        if connected and (time.time() - last_signal_time > TIMEOUT):
+        if connected and time.time() - last_signal_time > TIMEOUT:
             connected = False
             socketio.emit("update", {"connected": False})
         socketio.sleep(0.5)
